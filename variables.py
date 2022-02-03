400e7a953a5ad669885d72583b1a241bb8bd02d1
Ryan Burgin <ruiksilver@gmail.com>
2022-02-02 18:30:06 -0700

    added print statement examples

---
+++ variables.py
@@ -0,0 +1,15 @@
+# This is a comment
+# Variables in Python
+# By: Ryan Burgin
+# CS 1030
+
+# Variables
+
+first_name = 'Ryan'
+last_name = 'Burgin'
+
+print (first_name, last_name)
+print (first_name + last_name)
+print ('Mr. ' + first_name, last_name)
+
+
